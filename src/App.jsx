dc7412076204e42e05046e0175f8f8e06d3fda42
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-03 23:24:45 +0800

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
 import { useEffect, useState, useRef } from 'react';
+import './Card.css'
+
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+  steel: "#b3b3cc",
+  dark: "#33334d"
+};
 
 function randomized(min, max) { 
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -28,7 +50,13 @@ async function loadPokemon() {
     return {
       id: data.id,
       url: data.sprites.other.dream_world.front_default,
-      name: data.name 
+      name: data.name[0].toUpperCase() + data.name.slice(1),
+      type: data.types[0].type.name, 
+      statAttack: data.stats[1].base_stat,
+      statDefense: data.stats[2].base_stat,
+      statSpeed: data.stats[5].base_stat,
+      hp: data.stats[0].base_stat, 
+      data: data
     };
   } catch (error) {
     console.log(error);
@@ -38,20 +66,24 @@ async function loadPokemon() {
 function App() {
   const [isAssetLoaded, setAssetLoaded] = useState(false);
   const [arr, setArr] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clickedPokemon, setClickedPokemon] = useState(new Set());
+
   const hasFetched = useRef(false);
 
   console.log(arr)
+  console.log(clickedPokemon)
 
   useEffect(() => {
-    
     if (hasFetched.current) return;
     hasFetched.current = true;
 
     async function fetchPokemons() {
-      console.log("ftechin")
+      console.log("Fetching Pokémon")
       let array = [];
       let fetchedIds = new Set();
-  
+
       while (array.length < 10) {
         const pokemon = await loadPokemon();
 
@@ -68,19 +100,69 @@ function App() {
     fetchPokemons();
   }, []);
 
+  function handlePokemonClick(id) {
+    if (clickedPokemon.has(id)) {
+      setScore(0);
+      setClickedPokemon(new Set());
+    } else {
+      setScore((prevScore) => {
+        const newScore = prevScore + 1;
+        if (newScore > bestScore) setBestScore(newScore);
+        return newScore;
+      });
+      setClickedPokemon((prevSet) => new Set(prevSet).add(id));
+    }
+    setArr(shuffle(arr));
+  }
+
   return (
     <div>
       <h1>Memory Game</h1>
+      <h2>Score: {score}</h2>
+      <h2>Best Score: {bestScore}</h2>
       {!isAssetLoaded && <p>Loading...</p>}
+      <div className='container'>
       {isAssetLoaded &&
         arr.map((item) => (
-          <button key={item.id} onClick={() => {
-            setArr(shuffle(arr))
-          }}>
+          <button
+            key={item.id}
+            onClick={() => handlePokemonClick(item.id)}
+            id='card'
+            style={{
+              background: `radial-gradient(circle at 50% 0%, ${typeColor[item.data.types[0].type.name]} 36%, #ffffff 36%)`
+            }}
+          >
+            <p className="hp">
+              <span>HP</span>
+              {item.hp}
+            </p>
             <img src={item.url} alt={item.name} />
-            <h1>{item.name}</h1>
+            <h2 className="poke-name">{item.name}</h2>
+            <div className="types">
+              {item.data.types.map((type) => (
+                <span key={type.type.name} style={{ backgroundColor: typeColor[type.type.name] }}>
+                  {type.type.name}
+                </span>
+              ))}
+            </div>
+            <div className="stats">
+              <div>
+                <h3>{item.statAttack}</h3>
+                <p>Attack</p>
+              </div>
+              <div>
+                <h3>{item.statDefense}</h3>
+                <p>Defense</p>
+              </div>
+              <div>
+                <h3>{item.statSpeed}</h3>
+                <p>Speed</p>
+              </div>
+            </div>
           </button>
         ))}
+      </div>
+      
     </div>
   );
 }
